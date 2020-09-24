201413a8b318de941dff131a3cd91451f81093df
Fardin <fardin0855@gmail.com>
2020-09-24 13:49:37 +0200

    Update login.php

--- h7/login.php
+++ h7/login.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if(isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] == true) {
+if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {
     header("Location: dashboard.php");
 }
 
