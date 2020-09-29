1b7649f1ee1f36a156ad0648eaa7e7ef34541ad3
Fardin <fardin0855@gmail.com>
2020-09-29 14:23:10 +0200

    Add files via upload

--- h8/opdr3/Auto.php
+++ h8/opdr3/Auto.php
@@ -1,17 +1,40 @@
 <?php
 
-interface AutoOverzicht {
-    public function voegAutoToe();
-    public function getGefilterdeLijst();
-    public function getAutoLijst();
+class AutoOverzicht {
+    public array $autoos;
+
+    public function __construct() {
+        $this->autoos = [];
+    }
+
+    public function getGefilterdeLijst($minprijs, $maxprijs, $merk) {
+        $gefilterdeLijst = [];
+
+        foreach ($this->autoos as $auto) {
+            if ($auto->getPrijs() >= $minprijs && $auto->getPrijs() <= $maxprijs && $auto->getMerk() === $merk) {
+                $gefilterdeLijst[] = $auto;
+            } else if ($auto->getPrijs() >= $minprijs && $auto->getPrijs() <= $maxprijs && $merk === "") {
+                $gefilterdeLijst[] = $auto;
+            }
+        }
+
+        return $gefilterdeLijst;
+    }
+
+    public function voegAutoToe($auto) {
+        $this->autoos[] = $auto;
+    }
+
+    public function getAutoLijst() {
+        return $this->autoos;
+    }
 }
 
-class Auto implements AutoOverzicht {
+class Auto {
     private string $merk;
     private string $type;
     private int $prijs;
     private string $url;
-    private array $auto;
 
     public function __construct(string $merk, string $type, int $prijs, string $url) {
         $this->merk = $merk;
@@ -20,16 +43,21 @@ class Auto implements AutoOverzicht {
         $this->url = $url;
     }
 
-    public function voegAutoToe() {
-        
+    public function getMerk() {
+        return $this->merk;
     }
 
-    public function getGefilterdeLijst() {
-        
+    public function getType() {
+        return $this->type;
     }
 
-    public function getAutoLijst() {
-        
+    public function getPrijs() {
+        return $this->prijs;
     }
+
+    public function getUrl() {
+        return $this->url;
+    }
+
 
 }
--- h8/opdr3/index.php
+++ h8/opdr3/index.php
@@ -1,8 +1,17 @@
 <?php
+
 include_once("Auto.php");
 
-$audi = new Auto("Audi", "Noder", 6666.66, "https://noder.net");
+$merk = isset($_GET["merk"]) ? $merk = $_GET["merk"] : "";
+$minPrice = isset($_GET["minPrice"]) && $_GET["minPrice"] ? $minprijs = $_GET["minPrice"] : 0;
+$maxPrice = isset($_GET["maxPrice"]) && $_GET["maxPrice"] ? $maxprijs = $_GET["maxPrice"] : 99999999;
 
+
+$autooverzicht = new AutoOverzicht();
+$autooverzicht->voegAutoToe(new Auto("Audi", "Type-1", 10000, "https://media2.autokopen.nl/afbeeldingen/audi-a3-sportback-303757-1920.jpg"));
+$autooverzicht->voegAutoToe(new Auto("Audi", "Type-3", 15000, "https://zerauto.nl/wp-content/uploads/sites/2/2020/05/Audi-Q5-50-TFSI-e-plug-in-hybride-2020-test-AutoRAI.nl-FOTO-027.jpg"));
+$autooverzicht->voegAutoToe(new Auto("Ford", "Type-7", 20000, "https://media2.autokopen.nl/afbeeldingen/ford-kuga-301114-1920.jpg"));
+$autooverzicht->voegAutoToe(new Auto("Toyota", "Type-9B", 40000, "https://t1-cms-1.images.toyota-europe.com/toyotaone/nlnl/Header_Cover_tcm-22-1943880.jpg"));
 
 ?>
 
@@ -22,14 +31,16 @@ $audi = new Auto("Audi", "Noder", 6666.66, "https://noder.net");
         <div class="container">
             <div class="columns is-centered">
                 <div class="column is-half">
-                    <form action="index.php" method="POST">
+                    <form action="index.php" method="GET">
                         <div class="field">
                             <label class="label">Selecteer een automerk</label>
                             <div class="control">
                                 <div class="select">
-                                    <select>
-                                        <option>---Selecteer---</option>
-                                        <option>Ford</option>
+                                    <select name="merk">
+                                        <option value="">---Alle automerken---</option>
+                                        <option value="Ford">Ford</option>
+                                        <option value="Audi">Audi</option>
+                                        <option value="Toyota">Toyota</option>
                                     </select>
                                 </div>
                             </div>
@@ -38,14 +49,14 @@ $audi = new Auto("Audi", "Noder", 6666.66, "https://noder.net");
                         <div class="field">
                             <label class="label">Minimum Prijs</label>
                             <div class="control">
-                                <input class="input" type="text" placeholder="Voer een prijs in euro's in">
+                                <input class="input" name="minPrice" type="text" placeholder="Voer een prijs in euro's in">
                             </div>
                         </div>
 
                         <div class="field">
                             <label class="label">Maximum Prijs</label>
                             <div class="control">
-                                <input class="input" type="text" placeholder="Voer een prijs in euro's in">
+                                <input class="input" name="maxPrice" type="text" placeholder="Voer een prijs in euro's in">
                             </div>
                         </div>
                         <button class="button">Submit</button>
@@ -55,10 +66,18 @@ $audi = new Auto("Audi", "Noder", 6666.66, "https://noder.net");
             </div>
 
             <!-- Auto column -->
-            <div class="column">
+            <div class="columns auto-column">
+                <div class="column">
+                    <?php       
+                        foreach ($autooverzicht->getGefilterdeLijst($minPrice, $maxPrice, $merk) as $auto) {
+                            echo $auto->getMerk() . " - " .$auto->getPrijs() . "<br />";
+                            echo '<img width=512 height=512 src="' . $auto->getUrl() . '"alt=""/> <br/>';
+                        } 
+                        ?>
 
+                    
+                </div>
             </div>
-
         </div>
     </section>
 </body>
